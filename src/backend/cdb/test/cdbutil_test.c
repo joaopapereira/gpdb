b950245cdbe4d60c162e438e353e0103f7549f63
Joao Pereira <joaopapereira@gmail.com>
2018-09-27 15:57:06 -0400

    Bananas

--- src/backend/cdb/test/cdbutil_test.c
+++ src/backend/cdb/test/cdbutil_test.c
@@ -3,58 +3,71 @@
 #include "utils/elog.h"
 
 
-PGDLLIMPORT sigjmp_buf *PG_exception_stack;
-PGDLLIMPORT ErrorContextCallback *error_context_stack;
-#define PG_RE_THROW()  \
-	pg_re_throw()
-
-void
-elog_start(const char *filename, int lineno, const char *funcname)
-{
-	
-}
-
-void
-elog_finish(int elevel, const char * fmt, ...)
-{
-
-}
-
-bool
-errstart(int elevel, const char * filename, int lineno, const char * funcname, const char * domain) {
-	return true;
-}
-void errfinish(int dummy,...) {
-	
-}
-
-int	errcode(int sqlerrcode) {
-	return 0;	
-}
-
-int errmsg(const char *fmt,...) {
-	return 0;
-}
-void pg_re_throw(void) {}
-int errdetail(const char *fmt,...) {}
-void pre_format_elog_string(int errnumber, const char *domain) {}
-char *format_elog_string(const char *fmt,...){}
-void elog_internalerror(const char *filename, int lineno, const char *funcname){}
-int	errcode_for_file_access(void){}
-int
-errhint(const char *fmt,...){}
-int
-errcontext_msg(const char *fmt,...){}
-void
-write_stderr(const char *fmt,...){}
-void FlushErrorState(void){}
-void EmitErrorReport(void){}
+//PGDLLIMPORT sigjmp_buf *PG_exception_stack;
+//PGDLLIMPORT ErrorContextCallback *error_context_stack;
+//#define PG_RE_THROW()  \
+//	pg_re_throw()
+//
+//void
+//elog_start(const char *filename, int lineno, const char *funcname)
+//{
+//	
+//}
+//
+//void
+//elog_finish(int elevel, const char * fmt, ...)
+//{
+//
+//}
+//
+//bool
+//errstart(int elevel, const char * filename, int lineno, const char * funcname, const char * domain) {
+//	return true;
+//}
+//void errfinish(int dummy,...) {
+//	
+//}
+//
+//int	errcode(int sqlerrcode) {
+//	return 0;	
+//}
+//
+//int errmsg(const char *fmt,...) {
+//	return 0;
+//}
+//void pg_re_throw(void) {}
+//int errdetail(const char *fmt,...) {}
+//void pre_format_elog_string(int errnumber, const char *domain) {}
+//char *format_elog_string(const char *fmt,...){}
+//void elog_internalerror(const char *filename, int lineno, const char *funcname){}
+//int	errcode_for_file_access(void){}
+//int
+//errhint(const char *fmt,...){}
+//int
+//errcontext_msg(const char *fmt,...){}
+//void
+//write_stderr(const char *fmt,...){}
+//void FlushErrorState(void){}
+//void EmitErrorReport(void){}
 
 #include <cgreen/cgreen.h>
 #include <cgreen/mocks.h>
 #include <cdb/cdbvars.h>
 
 #include "cdb/cdbutil.h"
+
+
+/*
+ * This method will emulate the real ExceptionalCondition
+ * function by re-throwing the exception, essentially falling
+ * back to the next available PG_CATCH();
+ */
+void
+_ExceptionalCondition(void *data)
+{
+	(void*)data;
+	PG_RE_THROW();
+}
 
 
 static int32 saved_segment_index;
@@ -80,6 +93,9 @@ Ensure(Cgreen, fails_this_test)
 Ensure(Cgreen, dbid_exists_raises_exception_when_not_run_in_qd)
 {
 	sleep(20);
+	always_expect(elog_start);
+	always_expect(elog_finish,
+		with_sideeffect(&_ExceptionalCondition, NULL));
 	GpIdentity.segindex = 1;
 	PG_TRY();
 			{
--- src/backend/mock.mk
+++ src/backend/mock.mk
@@ -133,7 +133,7 @@ WRAP_FUNCS=$(addprefix $(WRAP_FLAGS), \
 %.t: $(OBJFILES) $(CMOCKERY_OBJS) $(MOCK_OBJS) %_test.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $(call WRAP_FUNCS, $(top_srcdir)/$(subdir)/test/$*_test.c) $(call BACKEND_OBJS, $(top_srcdir)/$(subdir)/$*.o $(patsubst $(MOCK_DIR)/%_mock.o,$(top_builddir)/src/%.o, $^)) $(filter-out %/objfiles.txt, $^) $(MOCK_LIBS) -o $@
 %.green: $(OBJFILES) $(MOCK_OBJS) %_test.o
-	$(CC) $(CFLAGS) $(LDFLAGS) -L/usr/local/lib/ $(call WRAP_FUNCS, $(top_srcdir)/$(subdir)/test/$*_test.c) $(call BACKEND_OBJS, $(top_srcdir)/$(subdir)/$*.o ../../../../src/backend/utils/error/elog.o $(top_srcdir)/backend/utils/error/elog.o) $(filter-out %/objfiles.txt, $^) $(MOCK_LIBS) -lcgreen -o $@
+	$(CC) $(CFLAGS) $(LDFLAGS) -L/usr/local/lib/ $(call WRAP_FUNCS, $(top_srcdir)/$(subdir)/test/$*_test.c) $(call BACKEND_OBJS, $(top_srcdir)/$(subdir)/$*.o $(patsubst $(MOCK_DIR)/%_mock.o,$(top_builddir)/src/%.o, $^)) $(filter-out %/objfiles.txt, $^) $(MOCK_LIBS) -lcgreen -o $@
 
 # We'd like to call only src/backend, but it seems we should build src/port and
 # src/timezone before src/backend.  This is not the case when main build has finished,
